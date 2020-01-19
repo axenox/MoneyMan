b61ffb6f55155f83d26eb4cee896d8183503b2a2
Andrej Kabachnik <andrej_ka@gmx.de>
2020-01-19 12:24:40 +0100

    FIX importing investments now works correctly

--- Actions/ImportInvestments.php
+++ Actions/ImportInvestments.php
@@ -1,64 +1,25 @@
 <?php
 namespace axenox\MoneyMan\Actions;
 
-use exface\Core\Actions\CreateData;
 use exface\Core\Interfaces\DataSources\DataTransactionInterface;
 use exface\Core\Interfaces\Tasks\ResultInterface;
 use exface\Core\Interfaces\Tasks\TaskInterface;
 use exface\Core\Factories\ResultFactory;
 use exface\Core\Interfaces\DataSheets\DataSheetInterface;
 use exface\Core\Factories\DataSheetFactory;
-use exface\Core\DataTypes\SortingDirectionsDataType;
-use exface\Core\Exceptions\Actions\ActionRuntimeError;
-use exface\Core\Exceptions\Actions\ActionInputError;
-use exface\Core\DataTypes\NumberDataType;
-use exface\Core\DataTypes\DateDataType;
-use exface\Core\DataTypes\ComparatorDataType;
 
 class ImportInvestments extends ImportTransactions
 {
     private $investmentData = null;
     
-    private $accountData = null;
-    
-    private $existingTransactions = null;
-    
     /**
-     * 
+     *
      * {@inheritDoc}
      * @see \exface\Core\Actions\CreateData::perform()
      */
     protected function perform(TaskInterface $task, DataTransactionInterface $transaction) : ResultInterface
-    {
+    {        
         $importData = $this->enrichInvestmentData($this->getInputDataSheet($task));
-        
-        $tsSheet = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.MoneyMan.transaction');
-        $tsSheet->getColumns()->addFromUidAttribute();
-        foreach ($importData->getRows() as $rowNr => $row) {            
-            $tsSheet->removeRows();
-            $tsSheet->addRow([
-                'date' => $row['transaction__date'],
-                'account' => $this->getAccountId($importData, $rowNr),
-                'transfer_transaction__account' => $this->getCashAccountId($importData, $rowNr),
-                'amount_booked' => $this->getAmountShareValue($importData, $rowNr),
-                'transfer_transaction__amount_booked' => (-1)*$this->getAmountPayed($importData, $rowNr),
-                'currency_booked' => $row['currency'],
-                'transaction_category' => $this->getTransactionCategories($importData, $rowNr),
-                'status' => 'C'
-            ]);
-            $tsSheet = $this->enrichData($tsSheet);
-            $tsSheet = $this->checkDuplicates($tsSheet);
-            $tsSheet->dataCreate(false, $transaction);
-            
-            $importData->setCellValue('transaction', $rowNr, $tsSheet->getUidColumn()->getCellValue(0));
-        }
-        
-        foreach ($importData->getColumns() as $col) {
-            if ($col->getAttribute()->getRelationPath()->isEmpty() === false) {
-                $importData->getColumns()->remove($col);
-            }
-        }
-        
         $affected_rows = $importData->dataCreate(false, $transaction);
         
         $result = ResultFactory::createDataResult($task, $importData, 'OK!');
@@ -82,64 +43,6 @@ class ImportInvestments extends ImportTransactions
         return $sheet;
     }
     
-    protected function getAmountShareValue(DataSheetInterface $importData, int $rowNr)
-    {
-        $shares = NumberDataType::cast($importData->getCellValue('shares', $rowNr));
-        $price = NumberDataType::cast($importData->getCellValue('price', $rowNr));
-        
-        if ($shares === null || $price === null) {
-            throw new ActionInputError($this, 'Missing input values: number of shares, price per share and total ar all required!');
-        }
-        
-        return $shares * $price;
-    }
-    
-    protected function getAmountPayed(DataSheetInterface $importData, int $rowNr) : float
-    {
-        $total = NumberDataType::cast($importData->getCellValue('transaction__amount_booked', $rowNr));
-        
-        if ($total === null) {
-            throw new ActionInputError($this, 'Missing input for total payed value!');
-        }
-        
-        return $total;
-    }
-    
-    protected function getTransactionCategories(DataSheetInterface $importData, int $rowNr) : DataSheetInterface
-    {
-        $ds = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.MoneyMan.transaction_category');
-        
-        $fee = $this->getAmountPayed($importData, $rowNr) - $this->getAmountShareValue($importData, $rowNr);
-        
-        $ds->addRow([
-            'category' => $this->getFeeCategoryId($importData, $rowNr),
-            'amount' => (-1)*$fee,
-            'transaction' => $importData->getCellValue('transaction', $rowNr)
-        ]);
-        
-        return $ds;
-    }
-    
-    protected function getInvestmentGainData(DataSheetInterface $importData, int $rowNr) : DataSheetInterface
-    {
-        $shares = $importData->getCellValue('shares', $rowNr);
-        if ($shares > 0) {
-            return null;
-        }
-        
-        $ds = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.MoneyMan.investment_transaction_gain');
-        
-        $fee = $this->getAmountPayed($importData, $rowNr) - $this->getAmountShareValue($importData, $rowNr);
-        
-        $ds->addRow([
-            'category' => $this->getFeeCategoryId($importData, $rowNr),
-            'amount' => (-1)*$fee,
-            'transaction' => $importData->getCellValue('transaction', $rowNr)
-        ]);
-        
-        return $ds;
-    }
-    
     protected function getInvestmentId(DataSheetInterface $importData, int $rowNr) : int
     {
         if ($this->investmentData === null) {
@@ -154,46 +57,9 @@ class ImportInvestments extends ImportTransactions
         $id = $this->investmentData->getCellValue('id', $this->investmentData->getColumns()->get('wkn')->findRowByValue($wkn));
         
         if ($id === null) {
-            throw new ActionInputError($this, 'Cannot find investment with WKN "' . $wkn . '"!');
+            throw new BehaviorRuntimeError($this->getObject(), 'Cannot find investment with WKN "' . $wkn . '"!');
         }
         
         return $id;
-    }
-    
-    protected function getFeeCategoryId(DataSheetInterface $importData, int $rowNr) : int
-    {
-        $accountRow = $this->getAccountData($importData, $rowNr);
-        return $accountRow['fee_category'];
-    }
-    
-    protected function getGainCategoryId(DataSheetInterface $importData, int $rowNr) : int
-    {
-        $accountRow = $this->getAccountData($importData, $rowNr);
-        return $accountRow['gain_category'];
-    }
-    
-    protected function getAccountId(DataSheetInterface $importData, int $rowNr) : int
-    {
-        $accountRow = $this->getAccountData($importData, $rowNr);
-        return $accountRow['account'];
-    }
-    
-    protected function getCashAccountId(DataSheetInterface $importData, int $rowNr) : int
-    {
-        $accountRow = $this->getAccountData($importData, $rowNr);
-        return $accountRow['cash_account'];
-    }
-    
-    protected function getAccountData(DataSheetInterface $importData, int $rowNr) : array
-    {
-        if ($this->accountData === null) {
-            $ds = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.MoneyMan.investment_account');
-            $ds->getColumns()->addMultiple(['id', 'account', 'cash_account', 'fee_category', 'gain_category']);
-            $ds->addFilterInFromString('account', $importData->getColumns()->get('transaction__account')->getValues());
-            $ds->dataRead();
-            $this->accountData = $ds;
-        }
-        
-        return $this->accountData->getRow($this->accountData->getColumns()->get('account')->findRowByValue($importData->getCellValue('transaction__account', $rowNr)));
     }
 }
--- Behaviors/InvestmentTransactionBehavior.php
+++ Behaviors/InvestmentTransactionBehavior.php
@@ -11,7 +11,7 @@ use exface\Core\Factories\DataSheetFactory;
 use exface\Core\Exceptions\Behaviors\BehaviorRuntimeError;
 use exface\Core\DataTypes\ComparatorDataType;
 use exface\Core\DataTypes\SortingDirectionsDataType;
-use exface\Core\DataTypes\DataSheetDataType;
+use exface\Core\DataTypes\NumberDataType;
 
 /**
  * 
@@ -21,6 +21,8 @@ use exface\Core\DataTypes\DataSheetDataType;
  */
 class InvestmentTransactionBehavior extends AbstractBehavior
 {    
+    private $accountData = null;
+    
     public function register() : BehaviorInterface
     {
         $this->getWorkbench()->eventManager()->addListener(OnBeforeCreateDataEvent::getEventName(), [$this, 'handleBeforeCreateData']);
@@ -30,27 +32,99 @@ class InvestmentTransactionBehavior extends AbstractBehavior
         return $this;
     }
     
-    public function handleBeforeCreateData(AbstractDataSheetEvent $event)
+    public function handleBeforeCreateData(OnBeforeCreateDataEvent $event)
     {
-        $txSheet = $event->getDataSheet();
+        $eventData = $event->getDataSheet();
         
-        if ($txSheet->getMetaObject()->is($this->getObject()) === false) {
+        if ($eventData->getMetaObject()->is($this->getObject()) === false) {
             return;
         }
         
-        foreach ($txSheet->getRows() as $rowNr => $row) {
-            $shares = $row['shares'];
-            if ($shares < 0) {
-                $sellSheet = $this->createSellSheet($txSheet, $rowNr);
-                if ($sellSheet->isEmpty() === true) {
-                    throw new BehaviorRuntimeError($this->getObject(), 'Cannot create sell-transactions');
-                }
-                if (! $col = $txSheet->getColumns()->getByExpression('investment_transaction_gain[sell_transaction]')){
-                    $col = $txSheet->getColumns()->addFromExpression('investment_transaction_gain[sell_transaction]');
-                }
-                $col->setValue($rowNr, $sellSheet);
+        // Check if the data originates from this behavior already and thus does not need
+        // further processing - see end of this method, where this special column is set.
+        if ($eventData->getCellValue('_bypass_InvestmentTransactionBehavior', 0) === true) {
+            return;
+        }
+        
+        $transaction = $event->getTransaction();
+        
+        $eventData->getSorters()->addFromString('transaction__date', SortingDirectionsDataType::ASC);
+        $eventData->sort();
+        
+        $accountTransferData = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.MoneyMan.transaction');
+        $accountTransferData->getColumns()->addFromUidAttribute();
+        
+        $investmentTxData = $eventData->copy();
+        foreach ($investmentTxData->getColumns() as $col) {
+            if ($col->getAttribute()->getRelationPath()->isEmpty() === false) {
+                $investmentTxData->getColumns()->remove($col);
             }
         }
+        
+        foreach ($eventData->getRows() as $rowNr => $row) {
+            $shares = NumberDataType::cast($row['shares']);
+            $price = NumberDataType::cast($row['price']);
+            $isSell = ($shares < 0);
+            
+            $accountTransferData->removeRows();
+            $investmentTxData->removeRows();
+            
+            foreach ($investmentTxData->getColumns() as $col) {
+                if ($eventCol = $eventData->getColumns()->get($col->getName())) {
+                    $col->setValue(0, $eventCol->getCellValue($rowNr));
+                }
+            }
+            
+            // If it is a sell-transaction, we need to add the buy-sell mapping
+            if ($isSell === true) {
+                $buyData = $this->getBuyTransactions($row['investment']);
+                $buySellData = $this->createBuySellSheet($buyData, (-1)*$shares);
+                if ($buySellData->isEmpty() === true) {
+                    throw new BehaviorRuntimeError($this->getObject(), 'Cannot create sell-transactions');
+                }
+                if (! $col = $investmentTxData->getColumns()->getByExpression('investment_transaction_sold[sell_transaction]')){
+                    $col = $investmentTxData->getColumns()->addFromExpression('investment_transaction_sold[sell_transaction]');
+                }
+                $col->setValue(0, $buySellData);
+                $totalGain = $this->getGainTotal($buyData, (-1)*$shares, $price);
+            } else {
+                $buySellData = null;
+                $totalGain = null;
+            }
+            
+            // Create the transfer-transactions between accounts
+            $accountTransferData->removeRows();
+            $accountTransferData->addRow([
+                'date' => $row['transaction__date'],
+                'account' => $this->getAccountId($eventData, $rowNr),
+                'transfer_transaction__account' => $this->getCashAccountId($eventData, $rowNr),
+                // The amount booked in the investment transaction is the value of the shares bought. This
+                // makes sure, there is no value accumulated in the account! This means 
+                // - for buy-transactions it is price*qty
+                // - for sell-transactions it is price*qty of the corresponding buy-transactions or
+                // sell-value - gain (since sell-value is actually negative, it is sell-value + gain
+                // really).
+                'amount_booked' => $this->getAmountShareValue($eventData, $rowNr) + ($totalGain ?? 0),
+                // The amount transferred to/from the cash-account is whatever is really being payed -
+                // including fees, etc.
+                'transfer_transaction__amount_booked' => (-1)*$this->getAmountPayed($eventData, $rowNr),
+                'currency_booked' => $row['currency'],
+                'transaction_category' => $this->createCategoriesSheet($eventData, $rowNr, $totalGain),
+                'status' => 'C'
+            ]);
+            $accountTransferData->dataCreate(false, $transaction);
+            
+            // Add transaction id to investment transaction
+            $investmentTxData->setCellValue('transaction', 0, $accountTransferData->getUidColumn()->getCellValue(0));
+            
+            // Create the investment transaction
+            // Since this behavior will be triggered again, add a special column value to bypass
+            // the behavior (see beginning of this method for handling this column).
+            $investmentTxData->setCellValue('_bypass_InvestmentTransactionBehavior', 0, true);
+            $investmentTxData->dataCreate(false, $transaction);
+        }
+        
+        $event->preventCreate();
         
         return;
     }
@@ -68,24 +142,134 @@ class InvestmentTransactionBehavior extends AbstractBehavior
         return;
     }
     
-    protected function createSellSheet(DataSheetInterface $transactionsSheet, int $rowNr) : DataSheetInterface
+    protected function getAmountShareValue(DataSheetInterface $importData, int $rowNr)
     {
-        $toSell = (-1) * $transactionsSheet->getCellValue('shares', $rowNr);
-        $sellTxId = $transactionsSheet->getCellValue($transactionsSheet->getMetaObject()->getUidAttribute(), $rowNr);
-        $byTxSheet = $this->getBuyTransactions($transactionsSheet->getCellValue('investment', $rowNr));
-        $sellSheet = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.MoneyMan.investment_transaction_gain');
+        $shares = NumberDataType::cast($importData->getCellValue('shares', $rowNr));
+        $price = NumberDataType::cast($importData->getCellValue('price', $rowNr));
         
-        foreach ($byTxSheet->getRows() as $buyRow) {
-            if ($toSell <= 0) {
+        if ($shares === null || $price === null) {
+            throw new BehaviorRuntimeError($this->getObject(), 'Missing input values: number of shares, price per share and total ar all required!');
+        }
+        
+        return $shares * $price;
+    }
+    
+    protected function getAmountPayed(DataSheetInterface $importData, int $rowNr) : float
+    {
+        $total = NumberDataType::cast($importData->getCellValue('transaction__amount_booked', $rowNr));
+        
+        if ($total === null) {
+            throw new BehaviorRuntimeError($this->getObject(), 'Missing input for total payed value!');
+        }
+        
+        return $total;
+    }
+    
+    protected function createCategoriesSheet(DataSheetInterface $importData, int $rowNr, float $totalGain = null) : DataSheetInterface
+    {
+        $ds = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.MoneyMan.transaction_category');
+        
+        $fee = $this->getAmountPayed($importData, $rowNr) - $this->getAmountShareValue($importData, $rowNr);
+        
+        $ds->addRow([
+            'category' => $this->getFeeCategoryId($importData, $rowNr),
+            'amount' => (-1)*$fee,
+            'transaction' => $importData->getCellValue('transaction', $rowNr)
+        ]);
+        
+        if ($totalGain !== null) {
+            $ds->addRow([
+                'category' => $this->getGainCategoryId($importData, $rowNr),
+                'amount' => $totalGain,
+                'transaction' => $importData->getCellValue('transaction', $rowNr)
+            ]);
+        }
+        
+        return $ds;
+    }
+    
+    protected function getGainTotal(DataSheetInterface $buyData, int $sellShares, float $sellPrice) : float
+    {
+        $gainTotal = 0;
+        foreach ($buyData->getRows() as $buyRow) {
+            $sellSharesFromRow = max($buyRow['shares'], $sellShares);
+            $gainTotal += $sellPrice * $sellSharesFromRow - $buyRow['price'] * $sellSharesFromRow;
+        }
+        return $gainTotal;
+    }
+    
+    protected function getInvestmentGainData(DataSheetInterface $importData, int $rowNr) : DataSheetInterface
+    {
+        $shares = $importData->getCellValue('shares', $rowNr);
+        if ($shares > 0) {
+            return null;
+        }
+        
+        $ds = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.MoneyMan.investment_transaction_sold');
+        
+        $fee = $this->getAmountPayed($importData, $rowNr) - $this->getAmountShareValue($importData, $rowNr);
+        
+        $ds->addRow([
+            'category' => $this->getFeeCategoryId($importData, $rowNr),
+            'amount' => (-1)*$fee,
+            'transaction' => $importData->getCellValue('transaction', $rowNr)
+        ]);
+        
+        return $ds;
+    }
+    
+    protected function getFeeCategoryId(DataSheetInterface $importData, int $rowNr) : int
+    {
+        $accountRow = $this->getAccountData($importData, $rowNr);
+        return $accountRow['fee_category'];
+    }
+    
+    protected function getGainCategoryId(DataSheetInterface $importData, int $rowNr) : int
+    {
+        $accountRow = $this->getAccountData($importData, $rowNr);
+        return $accountRow['gain_category'];
+    }
+    
+    protected function getAccountId(DataSheetInterface $importData, int $rowNr) : int
+    {
+        $accountRow = $this->getAccountData($importData, $rowNr);
+        return $accountRow['account'];
+    }
+    
+    protected function getCashAccountId(DataSheetInterface $importData, int $rowNr) : int
+    {
+        $accountRow = $this->getAccountData($importData, $rowNr);
+        return $accountRow['cash_account'];
+    }
+    
+    protected function getAccountData(DataSheetInterface $importData, int $rowNr) : array
+    {
+        if ($this->accountData === null) {
+            $ds = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.MoneyMan.investment_account');
+            $ds->getColumns()->addMultiple(['id', 'account', 'cash_account', 'fee_category', 'gain_category']);
+            $ds->addFilterInFromString('account', $importData->getColumns()->get('transaction__account')->getValues());
+            $ds->dataRead();
+            $this->accountData = $ds;
+        }
+        
+        return $this->accountData->getRow($this->accountData->getColumns()->get('account')->findRowByValue($importData->getCellValue('transaction__account', $rowNr)));
+    }
+    
+    protected function createBuySellSheet(DataSheetInterface $buyData, int $sellShares) : DataSheetInterface
+    {
+        $sellSheet = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.MoneyMan.investment_transaction_sold');
+        
+        foreach ($buyData->getRows() as $buyRow) {
+            if ($sellShares <= 0) {
                 break;
             }
             $sellable = $buyRow['shares_remaining'];
-            $sell = $sellable > $toSell ? $toSell : $sellable;
+            $sell = $sellable > $sellShares ? $sellShares : $sellable;
             $sellSheet->addRow([
                 'shares' => $sell,
                 'buy_transaction' => $buyRow['id']
             ]);
-            $toSell = $toSell - $sell;
+            $sellShares = $sellShares - $sell;
         }
         
         return $sellSheet;
@@ -96,7 +280,9 @@ class InvestmentTransactionBehavior extends AbstractBehavior
         $ds = DataSheetFactory::createFromObjectIdOrAlias($this->getWorkbench(), 'axenox.MoneyMan.investment_transaction');
         $ds->getColumns()->addMultiple([
             $ds->getMetaObject()->getUidAttributeAlias(),
-            'shares_remaining'
+            'shares_remaining',
+            'price',
+            'currency'
         ]);
         $ds->addFilterFromString('investment', $investmentId, ComparatorDataType::EQUALS);
         $ds->addFilterFromString('shares_remaining', 0, ComparatorDataType::GREATER_THAN);
