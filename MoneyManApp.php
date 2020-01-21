8e2975cef168fca251031bec2d7d2cdc705d8c40
Andrej Kabachnik <andrej_ka@gmx.de>
2020-01-21 13:24:24 +0100

    NEW SQL database installer added

---
+++ MoneyManApp.php
@@ -0,0 +1,34 @@
+<?php
+namespace axenox\MoneyMan;
+
+use exface\Core\Interfaces\InstallerInterface;
+use exface\Core\CommonLogic\Model\App;
+use exface\Core\Exceptions\Model\MetaObjectNotFoundError;
+use exface\Core\CommonLogic\AppInstallers\MySqlDatabaseInstaller;
+use exface\Core\Facades\AbstractHttpFacade\HttpFacadeInstaller;
+use exface\Core\Factories\FacadeFactory;
+use powerui\DemoCEP\Facades\UmsXmlApiFacade;
+
+class MoneyManApp extends App
+{
+    public function getInstaller(InstallerInterface $injected_installer = null)
+    {
+        $installer = parent::getInstaller($injected_installer);
+        
+        try {
+            $schema_installer = new MySqlDatabaseInstaller($this->getSelector());
+            $schema_installer
+            ->setDataSourceSelector('0x11e95e7b68dba8099677e4b318306b9a')
+            ->setFoldersWithMigrations(['InitDB','Migrations'])
+            ->setFoldersWithStaticSql('Views');
+            $installer->addInstaller($schema_installer);
+        } catch (MetaObjectNotFoundError $e) {
+            $this->getWorkbench()->getLogger()->warning('Cannot init SqlSchemInstaller for app ' . $this->getAliasWithNamespace() . ': no model there yet!');
+        }
+        $tplInstaller = new HttpFacadeInstaller($this->getSelector());
+        $tplInstaller->setFacade(FacadeFactory::createFromString(UmsXmlApiFacade::class, $this->getWorkbench()));
+        $installer->addInstaller($tplInstaller);
+        
+        return $installer;
+    }
+}
